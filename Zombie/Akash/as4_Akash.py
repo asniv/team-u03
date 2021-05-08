ec2c1e348cf6404fdcca99fe398483cabae436bc
asniv <77938440+asniv@users.noreply.github.com>
2021-05-07 23:57:46 -0500

    Update as4_Akash.py
    Add complete Akash test code - submission Player code and test Zombie code (slightly different from Player)

--- Zombie/Akash/as4_Akash.py
+++ Zombie/Akash/as4_Akash.py
@@ -4,6 +4,8 @@ import math
 from gamelib import *
 
 class ZombieCharacter(ICharacter):
+    ## Original zombie code begins here
+    
     # def __init__(self, obj_id, health, x, y, map_view):
     #     ICharacter.__init__(self, obj_id, health, x, y, map_view)
 
@@ -28,16 +30,14 @@ class ZombieCharacter(ICharacter):
         #     y_off = 0
 
         # return MoveEvent(self, x + x_off, y + y_off)
+        
+    ## Original zombie code ends there
     
     
     
     def __init__(self, obj_id, health, x, y, map_view):
         ICharacter.__init__(self, obj_id, health, x, y, map_view)
         # You may add any instance attributes you find useful to save information between frames
-        # self.ctr = self.RoundCounter()
-        # self.ctr = self.Counter()
-        # self.dam = self.Damage()
-        self.hey = 0
         
         # Storing records of actions between turns
         
@@ -89,17 +89,19 @@ class ZombieCharacter(ICharacter):
             id_pos = [] # initialize
             for target in scan_results: # for each scanned zombie
                 target_id = target.getID() # get target's ID
-                # print('zombie_health:', target_id.getHealth())
-                # print('fam look it\'s zombie', target)
                 
                 if target_id == 0:
-                    target_pos = target.getPos() # get target's position
+                    # get target's position
+                    target_pos = target.getPos()
+                    # get x and y coords of target
                     target_pos_x, target_pos_y = target_pos
+                    # get x and y coords of object position
                     obj_pos_x, obj_pos_y = obj_pos
                     dist = math.hypot(obj_pos_x - target_pos_x, obj_pos_y - target_pos_y) # get distance from attacker to target
-                    scale_factor = 1.0 / math.exp(dist) # attack damage scale factor
-                    id_pos.append([target_id, scale_factor, target_pos]) # append the current target ID and damage scale factor to the collection
-                    # print('We can see Player {}'.format(target_id))
+                    # attack damage scale factor
+                    scale_factor = 1.0 / math.exp(dist)
+                    # append the current target ID and damage scale factor to the collection
+                    id_pos.append([target_id, scale_factor, target_pos])
                 
             id_pos.sort(key = lambda x: x[1], reverse=True) # sort list from highest to lowest attack scale factor
             
@@ -113,20 +115,16 @@ class ZombieCharacter(ICharacter):
             '''
             
             '''
-            # print('MOVEMENT')
-            
             x_off, y_off = 4, 4 # initialize offset in x- and y-coordinates
             # start with invalid offsets to initialize while loop
             
             while abs(x_off) + abs(y_off) > 3: # while the sum of the offsets is invalid
                 x_off = random.randint(-3, 3) # randomly select a valid x-offset
                 y_off = random.randint(-3, 3) # randomly select a valid y-offset
-                # print('hey:', abs(x_off)+abs(y_off))
                 if abs(x_off) + abs(y_off) <= 3: # if the sum is valid
                     continue # then leave the while loop and continue
     
             # Check the bounds
-            # map_view = map_view # 
             size_x, size_y = map_view.getMapSize() # get the dimensions of the map
             x, y = obj_pos # get the player's current position
             if x + x_off < 0 or x + x_off >= size_x: # if the new x is invalid
@@ -153,10 +151,7 @@ class ZombieCharacter(ICharacter):
         # Count the current round number and rounds since each event
         # Game starts on round 1, with "0" rounds since each event
         add_round(self)
-        print_rounds(self)
-        
-        # self.hey += 1
-        # print('yo', self.hey)
+        # print_rounds(self)
         
         # Need to Scan for the first time
         # Otherwise the rest of the program, ScanEvent if we've just done a MoveEvent
@@ -165,7 +160,6 @@ class ZombieCharacter(ICharacter):
                 # print('SCAN 53!')
                 reset_round(self, 'scan') # note that we're doing a ScanEvent
                 return ScanEvent(self)
-        
         
         ## Order of Priority
         # Perform initial ScanEvent (only round 1)
@@ -178,10 +172,6 @@ class ZombieCharacter(ICharacter):
                 # If we can't damage them much
                     # Move elsewhere
         # ScanEvent for zombies
-        # 
-        # Check health
-            
-            # 1-1. If health is below maximum possible attack damage, then heal
        
         scan_results = self.getScanResults()
         obj_id = self.getID()
@@ -197,7 +187,7 @@ class ZombieCharacter(ICharacter):
             
             if obj_id == 0: # If we're the player
                 opponent_max_health = 100 # show zombies' max health
-                damage_threshold = id_pos[0][1]*opponent_max_health + self.getInitHealth() * 0.02
+                damage_threshold = id_pos[0][1]*50 + self.getInitHealth() * 0.02
             else: # If we're a zombie
                 opponent_max_health = 1000 # show player's max health
                 damage_threshold = 0.05*self.getInitHealth()
@@ -256,24 +246,16 @@ class ZombieCharacter(ICharacter):
                 reset_round(self, 'scan') # note that we're doing a ScanEvent
                 # print('SCAN 129!')
                 return ScanEvent(self) # then ScanEvent
-            
-            
-        print('hahaha')
-    
-        
-    
-        
-    
+                
+    print('"Brains." idk im not a zombie')
 
 
 class PlayerCharacter(ICharacter):
     def __init__(self, obj_id, health, x, y, map_view):
         ICharacter.__init__(self, obj_id, health, x, y, map_view)
         # You may add any instance attributes you find useful to save information between frames
-        # self.ctr = self.RoundCounter()
-        # self.ctr = self.Counter()
-        # self.dam = self.Damage()
-        self.hey = 0
+        
+        ## Thank you for a great class ##
         
         # Storing records of actions between turns
         
@@ -281,10 +263,10 @@ class PlayerCharacter(ICharacter):
         self.num_of = {'start': 0, 'attack': 0, 'heal': 0, 'move': 0, 'scan': 0} # number of times we've done event
         # rounds since the previous run of each event
         self.since_previous = {'start': 0, 'attack': 0, 'heal': 0, 'move': 0, 'scan': 0} # rounds since event
-        # if Player is allowed to [attack, heal, scan] (no: 0 or yes: 1)
-        self.toggle = {'start': 0, 'attack': 1, 'heal': 1, 'move': 1, 'scan': 1} # toggle to allow event
-        # next position for movement
-        self.next_pos = (0, 0)         
+        # # if Player is allowed to [attack, heal, scan] (no: 0 or yes: 1)
+        # self.toggle = {'start': 0, 'attack': 1, 'heal': 1, 'move': 1, 'scan': 1} # toggle to allow event
+        # # next position for movement
+        # self.next_pos = (0, 0)
         
 
     def selectBehavior(self):
@@ -320,25 +302,29 @@ class PlayerCharacter(ICharacter):
             Args:
                 scan_results: info taken from object Character's ScanData
                 obj_pos: the object Character's current position on the board
+                
+            Returns:
+                id_pos: list of lists of opponent info:
+                    [ID, scale_factor of damage we can inflict, (target_pos_x, target_pos_y)]
             '''
             
             id_pos = [] # initialize
-            for target in scan_results: # for each scanned zombie
+            for target in scan_results: # for each scanned opponent
                 
                 target_id = target.getID() # get target's ID
-                # print('zombie_health:', target_id.getHealth())
-                # print('fam look it\'s zombie', target)
-                target_pos = target.getPos() # get target's position
+                # Get target's position
+                target_pos = target.getPos() 
                 target_pos_x, target_pos_y = target_pos
+                # Get object's (current person's) position
                 obj_pos_x, obj_pos_y = obj_pos
+                # Distance from object to target
                 dist = math.hypot(obj_pos_x - target_pos_x, obj_pos_y - target_pos_y) # get distance from attacker to target
-                scale_factor = 1.0 / math.exp(dist) # attack damage scale factor
-                id_pos.append([target_id, scale_factor, target_pos]) # append the current target ID and damage scale factor to the collection
-                # print('We can see Zombie {}'.format(target_id))
+                # Attack damage scale factor
+                scale_factor = 1.0 / math.exp(dist)
+                # Append the current target ID and damage scale factor to the collection
+                id_pos.append([target_id, scale_factor, target_pos])
                 
             id_pos.sort(key = lambda x: x[1], reverse=True) # sort list from highest to lowest attack scale factor
-            
-            # print(id_pos)
             
             return id_pos
         
@@ -346,10 +332,15 @@ class PlayerCharacter(ICharacter):
         # Function to control Player movement
         def movement(self, map_view, obj_pos): # takes MapView and Pos objects from Player
             '''
+            Move the Character randomly within 3 Manhattan steps (the rule-defined maximum offset)
             
+            Args:
+                map_view: the map dimensions to provide boundaries
+                obj_pos: Character's current position to provide a reference point
+                
+            Returns:
+                tuple: (x_off, y_off) coordinate offsets for the Character to be displaced from their current position
             '''
-            # print('MOVEMENT')
-            
             x_off, y_off = 4, 4 # initialize offset in x- and y-coordinates
             # start with invalid offsets to initialize while loop
             
@@ -372,6 +363,9 @@ class PlayerCharacter(ICharacter):
             return (x_off, y_off) # return the x and y offsets
         
         def print_rounds(self):
+            '''
+            Print the number of rounds, rounds since each Event, and number of iterations of each Event
+            '''
             rows = [ '\nround {}',
                      '{} since attack, {} since heal, {} since move, {} since scan',
                      '{} attacks, {} heals, {} moves, {} scans' ]
@@ -388,10 +382,7 @@ class PlayerCharacter(ICharacter):
         # Count the current round number and rounds since each event
         # Game starts on round 1, with "0" rounds since each event
         add_round(self)
-        print_rounds(self)
-        
-        # self.hey += 1
-        # print('yo', self.hey)
+        # print_rounds(self)
         
         # Need to Scan for the first time
         # Otherwise the rest of the program, ScanEvent if we've just done a MoveEvent
@@ -413,10 +404,6 @@ class PlayerCharacter(ICharacter):
                 # If we can't damage them much
                     # Move elsewhere
         # ScanEvent for zombies
-        # 
-        # Check health
-            
-            # 1-1. If health is below maximum possible attack damage, then heal
        
         scan_results = self.getScanResults()
         obj_id = self.getID()
@@ -434,15 +421,15 @@ class PlayerCharacter(ICharacter):
                 opponent_max_health = 100 # show zombies' max health
             else: # If we're a zombie
                 opponent_max_health = 1000 # show player's max health
-            # damage_threshold = id_pos[0][1]*opponent_max_health + self.getInitHealth() * 0.02
-            damage_threshold = 40
+            damage_threshold = id_pos[0][1]*opponent_max_health + self.getInitHealth() * 0.02
+            # damage_threshold = 30
             # find max factor of damage that a zombie could inflict given the positions, excluding factor of health
             # also account for health decrement of each round
             
         # If there are no scan results or no zombies nearby, set the user-defined safe health threshold to 0.25 of original health
         except (IndexError): # if no zombies are in range as of last scan
             # print('no zombies in range')
-            damage_threshold = self.getInitHealth() * 0.1
+            damage_threshold = self.getInitHealth() * 0.04
         
         # print('Damage threshold:', damage_threshold)
         
@@ -450,25 +437,27 @@ class PlayerCharacter(ICharacter):
         if (self.getHealth() <= damage_threshold) and (self.num_of['heal'] < 5):
             # If player health is below the safe health threshold and has healed fewer than 5 times so far
             reset_round(self, 'heal')
-            # print('HEAL 99!')
             return HealEvent(self)
         
         
         # Else (if health is above the safe level), either AttackEvent or MoveEvent based on to scan data
         else:
-            if self.since_previous['scan'] <= 4: # If it's been at most 2 rounds since ScanEvent
+            if self.since_previous['scan'] <= 3: # If it's been at most <#> rounds since ScanEvent
                 id_pos = calc(self, scan_results, obj_pos)
                 if len(id_pos) >= 1: # and if there is at least one zombie in the scan data
                     
-                    # print('Damage we can do', self.getHealth()*id_pos[0][1])
+                    obj_pos_x, obj_pos_y = obj_pos
+                    closest_target = id_pos[0]
+                    ct_pos_x, ct_pos_y = closest_target[2]
+                    dist_closest = math.hypot(obj_pos_x - ct_pos_x, obj_pos_y - ct_pos_y)
                     
-                    if self.getHealth()*id_pos[0][1] > 35: # If the damage we'll inflict is higher than the user-defined threshold
+                    # If the damage we'll inflict is higher than the user-defined minimum
+                    # or if we're within math.hypot(3, 2) away from target
+                    if self.getHealth()*id_pos[0][1] > 35 or dist_closest <= math.hypot(3, 4): 
                         reset_round(self, 'attack') # note that we're doing an AttackEvent
-                        print(id_pos[0][0])
-                        print('ATTACK 111!')
                         return AttackEvent(self, id_pos[0][0]) # then AttackEvent
-                    else: # Else, if our damage is not high enough
-                    # (but it's still been at most 2 rounds since last ScanEvent and we saw a zombie)
+                    else: # Else, if our damage is not high enough or we're too far
+                    # (but it's still been at most <#> rounds since last ScanEvent and we saw a zombie)
                         # Then we do a MoveEvent
                         x, y = obj_pos
                         x_off, y_off = movement(self, map_view, obj_pos)
@@ -478,111 +467,18 @@ class PlayerCharacter(ICharacter):
                         
                         # # Then we do an AttackEvent
                         # reset_round(self, 'attack') # note that we're doing a AttackEvent
-                        # return AttackEvent(id_pos[0][1])
+                        # return AttackEvent(id_pos[0][0])
                         
-                else: # If it's been at most 2 rounds since ScanEvent, but we see no zombie
+                else: # If it's been at most <#> rounds since ScanEvent, but we see no zombie
                     x, y = obj_pos
                     x_off, y_off = movement(self, map_view, obj_pos)
                     reset_round(self, 'move') # note that we're doing a MoveEvent
                     # print('MOVE 123!')
                     return MoveEvent(self, x + x_off, y + y_off) # then MoveEvent
             
-            # Else, if it's been more than 2 rounds since last ScanEvent (but we see no zombie), then ScanEvent
+            # Else, if it's been more than <#> rounds since last ScanEvent (but we see no zombie), then ScanEvent
             else:
                 reset_round(self, 'scan') # note that we're doing a ScanEvent
-                # print('SCAN 129!')
                 return ScanEvent(self) # then ScanEvent
             
-            
-        print('hahaha')
-        
-        
-        
-
-            
-
-        
-        
-    
-    ## Mind Palace
-        
-    # class Counter: # (obj_id):
-        
-    #     def __init__(self, num_of = {'start': 0, 'attack': 0, 'heal': 0, 'move': 0, 'scan': 0}, # number of times we've done event
-    #                        since_previous = {'start': 0, 'attack': 0, 'heal': 0, 'move': 0, 'scan': 0}, # rounds since event
-    #                        toggle = [1, 1, 1, 1, 1], # toggle to allow event
-    #                        next_pos = (0, 0)         # next position for movement
-    #                        ):
-
-        
-
-            
-                
-        # def __str__(self):
-        #     '''
-        #     Print a message showing the round number, rounds since each event, and number runs of each Events
-        #     '''            
-
-        
-    # class Damage:
-        
-    #     def __init__(self, scan_results=0, obj_pos=(0,0), map_view=(0,0)):
-    #         self.scan_results = scan_results
-    #         self.obj_pos = obj_pos
-    #         self.map_view = map_view
-            
-
-        
-
-            
-            
-            # Started an alternative movement controller here
-            
-            # x_off, y_off = 4, 4 # initialize offset in x- and y-coordinates
-            # # start with invalid offsets to initialize while loop
-
-            # # Check the bounds
-            # # map_view = map_view # 
-            # size_x, size_y = map_view.getMapSize() # get the dimensions of the map
-            # x, y = obj_pos # get the player's current position
-            
-            # while abs(x_off) + abs(y_off) > 3: # while the sum of the offsets is invalid
-            #     x_off = random.randint(-3, 3) # randomly select a valid x-offset
-            #     y_off = random.randint(-3, 3) # randomly select a valid y-offset
-            #     # print('hey:', abs(x_off)+abs(y_off))
-            #     if (abs(x_off) + abs(y_off) <= 3) and (): # if the sum is valid
-            #         continue # then leave the while loop and continue
-    
-
-
-            # if x + x_off < 0 or x + x_off >= size_x: # ff the 
-            #     x_off = 0
-            # if y + y_off < 0 or y + y_off >= size_y:
-            #     y_off = 0
-                
-            # return (x_off, y_off)
-        
-        
-
-        
-        
-                
-        # def add_since_attack(self):
-        #     self.round_counter += 1``
-            
-        # def stab(self):
-        #     self.attack_toggle = 0
-    
-        # def his_score(self):
-        #     self.hiswins += 1
-    
-        # @property
-        # def best (self):
-        #     return max ( [self.mywins, self.hiswins] )
-            
-        # def __str__(self):
-        #     return 'oh yeah dab on em {}'.format(self.round_counter)
-        
-    # def __str__(self):
-        # return 'bruuuuh'+
-        # pass
+        print('If this ever prints out, flushed emoji')
